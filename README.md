7ba983cb509e42ffb2831706415427e685734a56
sam <samsuanchen@gmail.com>
2013-11-08 16:44:49 +0800

    修訂 README

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ a sample application for ksanapc, to demonstrate calling yase and Backbone Aura,
 * cd slotfilter/sample
 * node sample
 you will get sample.ydb in slotfilter
+
+製作 bible_chinese.ydb 的步驟:
+1. 用 conv_chinese_bible.js 轉換 1ot.xm, 2nt.xml ==> 1ot1.xml, 2nt1.xml
+2. 編輯 chinese_bible.lst 包括兩列 1ot1.xml, 2nt1.xml
+3. 編輯 mvbr1.js 引用 chinese_bible.lst
+4. node mvbr1.js 建立 chinese_bible.ydb
+5. commit
+6. 到 kse 資料夾, 執行 git pull (確定使用最新版本)
+7. 到 ksanapc 資料夾, 執行 node scaffold.js mvbr kse --overwrite
+8. 到 mvbr 資料夾, 編輯 config.json 指定 db 使用 chinese_bible.ydb
+
+測試 bible_chinese.ydb 的步驟:
+1. 到 kse 資料夾, 執行 run.cmd
---
+++ xml/conv_hebrew_bible.js
@@ -0,0 +1,208 @@
+console.log(Date())
+var naming= // cn=chinese_name cb=chinese_brief en=english_name eb=english_brief
+{"1":{"cn":"創世記","cb":"創","en":"Genesis","eb":"Gen"}
+,"Gen":"1","Ge":"1","創":"1","Genesis":"1"
+,"2":{"cn":"出埃及記","cb":"出","en":"Exodus","eb":"Exo"}
+,"Exo":"2","出":"2","Exodus":"2"
+,"3":{"cn":"利未記","cb":"利","en":"Leviticus","eb":"Lev"}
+,"Lev":"3","利":"3","Leviticus":"3"
+,"4":{"cn":"民數記","cb":"民","en":"Numbers","eb":"Num"}
+,"Num":"4","民":"4","Numbers":"4"
+,"5":{"cn":"申命記","cb":"申","en":"Deuteronomy","eb":"Deu"}
+,"Deu":"5","申":"5","Deuteronomy":"5"
+,"6":{"cn":"約書亞記","cb":"書","en":"Joshua","eb":"Jos"}
+,"Jos":"6","Josh":"6","書":"6","Joshua":"6"
+,"7":{"cn":"士師記","cb":"士","en":"Judges","eb":"Jug"}
+,"Jug":"7","Jdgs":"7","士":"7","Judges":"7"
+,"8":{"cn":"路得記","cb":"得","en":"Ruth","eb":"Rut"}
+,"Rut":"8","Ruth":"8","得":"8","Ruth":"8"
+,"9":{"cn":"撒母耳記上","cb":"撒上","en":"1 Samuel","eb":"1Sa"}
+,"1Sa":"9","1Sm":"9","撒上":"9","1 Samuel":"9"
+,"10":{"cn":"撒母耳記下","cb":"撒下","en":"2 Samuel","eb":"2Sa"}
+,"2Sa":"10","2Sm":"10","撒下":"10","2 Samuel":"10"
+,"11":{"cn":"列王紀上","cb":"王上","en":"1 Kings","eb":"1Ki"}
+,"1Ki":"11","王上":"11","1 Kings":"11"
+,"12":{"cn":"列王紀下","cb":"王下","en":"2 Kings","eb":"2Ki"}
+,"2Ki":"12","王下":"12","2 Kings":"12"
+,"13":{"cn":"歷代志上","cb":"代上","en":"1 Chronicles","eb":"1Ch"}
+,"1Ch":"13","1Chr":"13","代上":"13","1 Chronicles":"13"
+,"14":{"cn":"歷代志下","cb":"代下","en":"2 Chronicles","eb":"2Ch"}
+,"2Ch":"14","2Chr":"14","代下":"14","2 Chronicles":"14"
+,"15":{"cn":"以斯拉記","cb":"拉","en":"Ezra","eb":"Ezr"}
+,"Ezr":"15","Ezra":"15","拉":"15","Ezra":"15"
+,"16":{"cn":"尼希米記","cb":"尼","en":"Nehemiah","eb":"Neh"}
+,"Neh":"16","尼":"16","Nehemiah":"16"
+,"17":{"cn":"以斯帖記","cb":"斯","en":"Esther","eb":"Est"}
+,"Est":"17","斯":"17","Esther":"17"
+,"18":{"cn":"約伯記","cb":"伯","en":"Job","eb":"Job"}
+,"Job":"18","伯":"18","Job":"18"
+,"19":{"cn":"詩篇","cb":"詩","en":"Psalms","eb":"Psm"}
+,"Psm":"19","Psa":"19","詩":"19","Psalms":"19"
+,"20":{"cn":"箴言","cb":"箴","en":"Proverbs","eb":"Pro"}
+,"Pro":"20","Prv":"20","箴":"20","Proverbs":"20"
+,"21":{"cn":"傳道書","cb":"傳","en":"Ecclesiastes","eb":"Ecc"}
+,"Ecc":"21","Eccl":"21","傳":"21","Ecclesiastes":"21"
+,"22":{"cn":"雅歌","cb":"歌","en":"Song of Songs","eb":"Son"}
+,"Son":"22","Sol":"22","歌":"22","Song of Songs":"22"
+,"23":{"cn":"以賽亞書","cb":"賽","en":"Isaiah","eb":"Isa"}
+,"Isa":"23","賽":"23","Isaiah":"23"
+,"24":{"cn":"耶利米書","cb":"耶","en":"Jeremiah","eb":"Jer"}
+,"Jer":"24","耶":"24","Jeremiah":"24"
+,"25":{"cn":"耶利米哀歌","cb":"哀","en":"Lamentations","eb":"Lam"}
+,"Lam":"25","哀":"25","Lamentations":"25"
+,"26":{"cn":"以西結書","cb":"結","en":"Ezekiel","eb":"Eze"}
+,"Eze":"26","結":"26","Ezekiel":"26"
+,"27":{"cn":"但以理書","cb":"但","en":"Daniel","eb":"Dan"}
+,"Dan":"27","但":"27","Daniel":"27"
+,"28":{"cn":"何西阿書","cb":"何","en":"Hosea","eb":"Hos"}
+,"Hos":"28","何":"28","Hosea":"28"
+,"29":{"cn":"約珥書","cb":"珥","en":"Joel","eb":"Joe"}
+,"Joe":"29","Joel":"29","珥":"29","Joel":"29"
+,"30":{"cn":"阿摩司書","cb":"摩","en":"Amos","eb":"Amo"}
+,"Amo":"30","Amos":"30","摩":"30","Amos":"30"
+,"31":{"cn":"俄巴底亞書","cb":"俄","en":"Obadiah","eb":"Oba"}
+,"Oba":"31","Obad":"31","俄":"31","Obadiah":"31"
+,"32":{"cn":"約拿書","cb":"拿","en":"Jonah","eb":"Jon"}
+,"Jon":"32","Jonah":"32","拿":"32","Jonah":"32"
+,"33":{"cn":"彌迦書","cb":"彌","en":"Micah","eb":"Mic"}
+,"Mic":"33","彌":"33","Micah":"33"
+,"34":{"cn":"那鴻書","cb":"鴻","en":"Nahum","eb":"Nah"}
+,"Nah":"34","Nahum":"34","鴻":"34","Nahum":"34"
+,"35":{"cn":"哈巴谷書","cb":"哈","en":"Habakkuk","eb":"Hab"}
+,"Hab":"35","哈":"35","Habakkuk":"35"
+,"36":{"cn":"西番雅書","cb":"番","en":"Zephaniah","eb":"Zep"}
+,"Zep":"36","番":"36","Zephaniah":"36"
+,"37":{"cn":"哈該書","cb":"該","en":"Haggai","eb":"Hag"}
+,"Hag":"37","該":"37","Haggai":"37"
+,"38":{"cn":"撒迦利亞書","cb":"亞","en":"Zechariah","eb":"Zec"}
+,"Zec":"38","亞":"38","Zechariah":"38"
+,"39":{"cn":"瑪拉基書","cb":"瑪","en":"Malachi","eb":"Mal"}
+,"Mal":"39","瑪":"39","Malachi":"39"
+,"40":{"cn":"馬太福音","cb":"太","en":"Matthew","eb":"mat"}
+,"Mat":"40","太":"40","Matthew":"40"
+,"41":{"cn":"馬可福音","cb":"可","en":"Mark","eb":"Mak"}
+,"Mak":"41","Mark":"41","可":"41","Mark":"41"
+,"42":{"cn":"路加福音","cb":"路","en":"Luke","eb":"Luk"}
+,"Luk":"42","Luke":"42","路":"42","Luke":"42"
+,"43":{"cn":"約翰福音","cb":"約","en":"John","eb":"Jhn"}
+,"Jhn":"43","John":"43","約":"43","John":"43"
+,"44":{"cn":"使徒行傳","cb":"徒","en":"Acts","eb":"Act"}
+,"Act":"44","Acts":"44","徒":"44","Acts":"44"
+,"45":{"cn":"羅馬書","cb":"羅","en":"Romans","eb":"Rom"}
+,"Rom":"45","羅":"45","Romans":"45"
+,"46":{"cn":"哥林多前書","cb":"林前","en":"1 Corinthians","eb":"1Co"}
+,"1Co":"46","1Cor":"46","林前":"46","1 Corinthians":"46"
+,"47":{"cn":"哥林多後書","cb":"林後","en":"2 Corinthians","eb":"2Co"}
+,"2Co":"47","2Cor":"47","林後":"47","2 Corinthians":"47"
+,"48":{"cn":"加拉太書","cb":"加","en":"Galatians","eb":"Gal"}
+,"Gal":"48","加":"48","Galatians":"48"
+,"49":{"cn":"以弗所書","cb":"弗","en":"Ephesians","eb":"Eph"}
+,"Eph":"49","弗":"49","Ephesians":"49"
+,"50":{"cn":"腓利比書","cb":"腓","en":"Philippians","eb":"Phl"}
+,"Phl":"50","Phi":"50","腓":"50","Philippians":"50"
+,"51":{"cn":"歌羅西書","cb":"西","en":"Colossians","eb":"Col"}
+,"Col":"51","西":"51","Colossians":"51"
+,"52":{"cn":"帖撒羅尼迦前書","cb":"帖前","en":"1 Thessalonians","eb":"1Ts"}
+,"1Ts":"52","1Th":"52","帖前":"52","1 Thessalonians":"52"
+,"53":{"cn":"帖撒羅尼迦後書","cb":"帖後","en":"2 Thessalonians","eb":"2Ts"}
+,"2Ts":"53","2Th":"53","帖後":"53","2 Thessalonians":"53"
+,"54":{"cn":"提摩太前書","cb":"提前","en":"1 Timothy","eb":"1Ti"}
+,"1Ti":"54","1Tim":"54","提前":"54","1 Timothy":"54"
+,"55":{"cn":"提摩太後書","cb":"提後","en":"2 Timothy","eb":"2Ti"}
+,"2Ti":"55","2Tim":"55","提後":"55","2 Timothy":"55"
+,"56":{"cn":"提多書","cb":"多","en":"Titus","eb":"Tit"}
+,"Tit":"56","Titus":"56","多":"56","Titus":"56"
+,"57":{"cn":"腓利門書","cb":"門","en":"Philemon","eb":"Phm"}
+,"Phm":"57","Phmn":"57","門":"57","Philemon":"57"
+,"58":{"cn":"希伯來書","cb":"來","en":"Hebrews","eb":"Heb"}
+,"Heb":"58","來":"58","Hebrews":"58"
+,"59":{"cn":"雅各書","cb":"雅","en":"James","eb":"Jas"}
+,"Jas":"59","雅":"59","James":"59"
+,"60":{"cn":"彼得前書","cb":"彼前","en":"1 Peter","eb":"1Pe"}
+,"1Pe":"60","1Pet":"60","彼前":"60","1 Peter":"60"
+,"61":{"cn":"彼得後書","cb":"彼後","en":"2 Peter","eb":"2Pe"}
+,"2Pe":"61","2Pet":"61","彼後":"61","2 Peter":"61"
+,"62":{"cn":"約翰壹書","cb":"約壹","en":"1 John","eb":"1Jn"}
+,"1Jn":"62","約壹":"62","1 John":"62"
+,"63":{"cn":"約翰貳書","cb":"約貳","en":"2 John","eb":"2Jn"}
+,"2Jn":"63","約貳":"63","2 John":"63"
+,"64":{"cn":"約翰參書","cb":"約參","en":"3 John","eb":"3Jn"}
+,"3Jn":"64","約參":"64","3 John":"64"
+,"65":{"cn":"猶大書","cb":"猶","en":"Jude","eb":"Jud"}
+,"Jud":"65","Jude":"65","猶":"65","Jude":"65"
+,"66":{"cn":"啟示錄","cb":"啟","en":"Revelation","eb":"Rev"}
+,"Rev":"66","啟":"66","Revelation":"66"
+} 
+// console.log(naming["猶"])
+var bmatch, cmatch, vmatch, line, N, i, j, n
+var bible, text, testment
+var bid, cid, vid, book_id, chapter_id, verse_id, verse
+var book_index=0, chapter_index, verse_index, book, books
+var backs='';for(var j=0;j<5;j++)backs+=String.fromCharCode(8)
+var fs=require('fs')
+var file_name, file, out
+var d2=function (n) {return (n<10?'0':'')+n}
+var f, dir='D:\\!!bible\\Hebrew-English\\pt\\ptnn.htm\\'
+var t='', nid='', c, last_name
+var out=['jps.txt Jewish Publication Society 1917 edition (www.mechon-mamre.org/pdbjps.htm)']
+for (f=1;;f++) {
+	file='pt'+(f<10?'0':'')+f+'.htm'
+	if (!fs.existsSync(dir+file)) break
+	text=fs.readFileSync(dir+file)
+	text=text.toString()
+	bmatch=text.match(/<H1.+?>(.+?) <span/)
+	book=bmatch[1]
+	text=text.split('\n<\/TD><\/TR>')
+//	console.log(file,book,text.length)
+	for (i=0;i<text.length;i++) {
+	//	if (i===3) break
+		if (cmatch=text[i].match(/<H2 ALIGN="CENTER">(. )?Chapter (.+)<\/H2>/)) {
+			nid=cmatch[1]
+			if (nid && isNaN(c=nid.substr(0,1))) {
+				books=book.split(' - ')
+				for (j=0;j<books.length;j++)
+					if (c===books[j].substr(0,1))
+						book_name=books[j]
+			} else {
+				book_name=(nid||'')+book
+			}
+			if (last_name!==book_name) {
+				if (last_name) {
+					console.log(last_name,t)
+					t=''
+				}
+				console.log(file,(i+1)+'/'+text.length,book)
+				last_name = book_name
+				book_index=naming[book_name]
+				N=naming[book_index]
+				bid=N.eb
+			}
+			cid=bid+cmatch[2]
+			t+=cmatch[2]+' '
+	//		console.log(cid)
+		}
+		if (vmatch=text[i].match(/<TR><TD class=h.*?>\n<B>(.+?)<\/B>(.+?)\n<\/TD><TD.*?>\n<B>(.+?)<\/B>(.+)$/)) {
+			vid=cid+':'+vmatch[3]
+			verse=vmatch[4]
+	//		verse=vmatch[4].replace(/<B>.+?<\/B>/g,'').replace('<BR>','')
+			out.push(vid+verse)
+	//		console.log(cid+':'+vmatch[3],verse)
+		}
+	}
+	fs.writeFileSync('jps.txt',out.join('\r\n'))
+	 /*
+//	if (cmatch[1]) console.log(match[1]+book)
+	nid=nid||''
+	book_name=nid+book, book_index=naming[book_name]
+	console.log('book_name',book_name)
+	N=naming[book_index], bid=N.eb
+	console.log(book_index,d2(book_index),bid,N.cn,book_name,t)
+//		}
+//	}
+/*	out.push('</xml>')
+	file=file_name+'1.xml'
+	console.log(out.length+' lines write to '+file)
+	
+*/
+}
+console.log(last_name,t); t=''
